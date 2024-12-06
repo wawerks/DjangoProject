44ae7d75b06609e67b3f99c86a74b8c03b8fce8b
prnsnnyo <princenino.antigo@carsu.edu.ph>
2024-12-06 10:01:49 +0800

    updated edit profile

--- DjangoProject-App1/a_users/models.py
+++ DjangoProject-App1/a_users/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    #  image = models.ImageField(upload_to='avatars/', null=True, blank=True)
     displayname = models.CharField(max_length=20, null=True, blank=True)
     info = models.TextField(null=True, blank=True) 
     
