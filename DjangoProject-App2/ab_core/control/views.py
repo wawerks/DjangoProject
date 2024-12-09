1a605fb8a7b4939c82b41c9e1818cce7bb0da14b
wawerks <rhowinlumales@gmail.com>
2024-12-09 21:16:02 +0800

    added a custom admin page

--- DjangoProject-App2/ab_core/control/views.py
+++ DjangoProject-App2/ab_core/control/views.py
@@ -3,10 +3,40 @@ from django.http import JsonResponse
 import requests
 from requests.exceptions import ConnectionError
 from django.conf import settings
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages  # Import messages module
+from .models import Message
 
 # Create your views here.
 
-from django.shortcuts import render
-
 def landing_page(request):
     return render(request, 'control/landing.html')
+
+def login_page(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            # Authenticate user
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                # Login the user
+                login(request, user)
+                return redirect('custom_admin')  # Redirect to the custom admin page (not default Django admin)
+            else:
+                messages.error(request, 'Invalid username or password.')
+
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'control/login.html', {'form': form})
+
+
+def custom_admin(request):
+    messages_list = Message.objects.all()
+    return render(request, 'control/custom_admin.html', {'messages': messages_list})
